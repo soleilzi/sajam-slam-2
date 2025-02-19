2c0337e58fb499bff47cc727b3091085eb45aaf1
zeshan <zeshanm.zaki@gmail.com>
2025-02-18 21:40:47 -0500

    production changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from "path"
 import { connectDB } from './config/db.js';
 import streamerRoutes from './routes/streamerRoutes.js'
 
@@ -8,9 +9,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/streamers", streamerRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
